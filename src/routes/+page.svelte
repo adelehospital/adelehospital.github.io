8379daf52671533bc90fcc4dbcafd9bc59054f2a
Félix Defrance <felixgif@gmail.com>
2023-07-24 15:53:42 +0200

    adds image
    Closes #2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,5 +10,22 @@
 </svelte:head>
 
 <section in:fade>
+	<img
+		src="/images/adele.webp"
+		alt="Adèle Hospital portant un chapeau de paille et un haut blanc, assise face à un champ de blé avec un forêt en arrière plan, le tout sous un beau ciel bleu."
+	/>
+
 	<svelte:component this={data.content} />
 </section>
+
+<style>
+	img {
+		height: 40vh;
+		padding-bottom: 1rem;
+		border-radius: 8px;
+	}
+
+	section {
+		text-align: center;
+	}
+</style>
